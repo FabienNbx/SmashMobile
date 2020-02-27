9c3232dd8f448b2d5c09313bd3a7af828c696804
SayMouz <faouz5307@gmail.com>
2020-02-27 10:53:16 +0100

    Add player attacks

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public FireBallMovement fireBall;
+
+    private PlayerMovements playerMovements;
+    private PlayerInput inputs;
+
+    void Start()
+    {
+        playerMovements = GetComponent<PlayerMovements>();
+        inputs = GetComponent<PlayerInput>();
+    }
+
+    void Update()
+    {
+        if(inputs.attack)
+        {
+           attack();
+        }
+    }
+
+    public void attack()
+    {
+        Vector3 sizePlayer = GetComponent<SpriteRenderer>().bounds.size; //Taille réelle du player
+        Vector3 sizeMissile = fireBall.GetComponent<SpriteRenderer>().bounds.size; //Taille réelle d'une fireBall
+        // Instancie un nouveau missile juste à côté du player en fonction de là où il regarde
+        FireBallMovement missile = Instantiate(fireBall, transform.position + new Vector3(playerMovements.look*(sizePlayer.x/2 + sizeMissile.x/2),0,0), transform.rotation);
+        Vector3 movement = new Vector3(playerMovements.look, 0, 0); //Indique la direction où on lance le missile
+        missile.launch(movement); //Lance le missile
+        missile.DestroyObjectDelayed();//Le missile est automatiquement détruit au bout d'un certain temps
+    }
+}
