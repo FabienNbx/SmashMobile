36c632f0747093260fdafdbd825f66d145e470ee
Zakaria <zakaria.nassreddine@insa-lyon.fr>
2020-02-27 11:42:13 +0100

    Commented health

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -5,34 +5,34 @@ using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public int startingHealth=100;
-    public int currentHealth;
-    public Slider healthSlider;
-    public Image DamageImage;
-    public float flashSpeed= 5f;
-    public Color flashColour = new Color(1f, 0f, 0f, 0.1f); 
+    public int startingHealth=100;  //santé de départ
+    public int currentHealth;       //santé courante
+    public Slider healthSlider;     //slider implémenté dans l'UI sur Unity
+    public Image DamageImage;       //image rouge qui flash sur l'écran
+    public float flashSpeed= 5f;    //vitesse du flash
+    public Color flashColour = new Color(1f, 0f, 0f, 0.1f);    //couleur de l'image rouge
     
-    private Animator anim;
-    private PlayerMovements playerMove;
-    private bool isDead;
-    private bool damaged;
+    private Animator anim;          //appel au component Animator
+    private PlayerMovements playerMove;     //appel au script PlayerMovements
+    private bool isDead;            //boolean mort
+    private bool damaged;           //boolean pris des dégats
 
     void Start ()
     {
         anim = GetComponent <Animator> ();
         playerMovement = GetComponent <PlayerMovements> ();
-        currentHealth = startingHealth;
+        currentHealth = startingHealth;     //initialiser la santé à la santé de départ
     }
 
     void Update ()
     {
         if(damaged)
         {
-            damageImage.color = flashColour;
+            damageImage.color = flashColour;    //si damage pris, flasher un écran rouge
         }
         else
         {
-            damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+            damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime); //paramétrage du flash
         }
         damaged = false;
     }
@@ -40,21 +40,21 @@ public class PlayerHealth : MonoBehaviour
     public void TakeDamage (int amount)
     {
         damaged = true;
-        currentHealth -= amount;
-        healthSlider.value = currentHealth;
+        currentHealth -= amount;            //décrémenter la santé
+        healthSlider.value = currentHealth; //ajuster la valeur du slider
 
-        if(currentHealth <= 0 && !isDead)
+        if(currentHealth <= 0 && !isDead)   //si santé atteint zéro et joueur pas déjà mort
         {
-            Death ();
+            Death ();                       //tuer le joueur
         }
     }
 
     void Death ()
     {
-        isDead = true;
-        anim.SetTrigger ("Die");
-        transform.position = new Vector2(0, 5);
-        currentHealth = startingHealth;
+        isDead = true;                      //mettre le flag mort à 1
+        anim.SetTrigger ("Die");            //animation de mort
+        transform.position = new Vector2(0, 5);     //respawn
+        currentHealth = startingHealth;     //réinitialiser la santé
     }
 
 
