8cf530a3c052e8e9dc09d32bb124d4368eda10e3
Polo <pablo_mo@orange.fr>
2020-02-27 11:22:15 +0100

    Merge map and player

--- Assets/Scripts/FireBallMovement.cs
+++ Assets/Scripts/FireBallMovement.cs
@@ -28,7 +28,7 @@ public class FireBallMovement : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D other)
     {
-        if(other.gameObject.CompareTag("ennemi"))
+        if(other.gameObject.CompareTag("ennemi") || other.gameObject.CompareTag("Terrain"))
         {
             Destroy(gameObject);
         }
