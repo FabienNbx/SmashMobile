daf3ab01e0ba27ed24db92c048e390595e0256a6
Zakaria <zakaria.nassreddine@insa-lyon.fr>
2020-03-12 11:33:16 +0100

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public enum GameState {INTRO, MENU, PLAYING, DEAD, GAMEOVER};
 public class GameManager : MonoBehaviour
